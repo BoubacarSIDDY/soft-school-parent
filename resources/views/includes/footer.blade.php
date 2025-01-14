92f51f39d2f2bfc0fa7475579e0a70e0987a7a1f
BoubacarSIDDY <dialloboubacarsiddy2004@gmail.com>
2025-01-14 00:11:50 +0000

    ajout du template

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+<div class="footer">
+    <div class="copyright">
+        <p>Développé par <a href="https://okytechnologies.com/" target="_blank">{{env('DEV')}}</a> <span class="current-year">{{date('Y')}}</span></p>
+    </div>
+</div>
---
+++ resources/views/includes/sidebar.blade.php
@@ -0,0 +1,112 @@
+<div class="deznav">
+    <div class="deznav-scroll">
+        <ul class="metismenu" id="menu">
+            <li class="menu-title">ESPACE PARENT</li>
+            <li class="mm-active">
+                <a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <div class="menu-icon">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M9.13478 20.7733V17.7156C9.13478 16.9351 9.77217 16.3023 10.5584 16.3023H13.4326C13.8102 16.3023 14.1723 16.4512 14.4393 16.7163C14.7063 16.9813 14.8563 17.3408 14.8563 17.7156V20.7733C14.8539 21.0978 14.9821 21.4099 15.2124 21.6402C15.4427 21.8705 15.756 22 16.0829 22H18.0438C18.9596 22.0024 19.8388 21.6428 20.4872 21.0008C21.1356 20.3588 21.5 19.487 21.5 18.5778V9.86686C21.5 9.13246 21.1721 8.43584 20.6046 7.96467L13.934 2.67587C12.7737 1.74856 11.1111 1.7785 9.98539 2.74698L3.46701 7.96467C2.87274 8.42195 2.51755 9.12064 2.5 9.86686V18.5689C2.5 20.4639 4.04738 22 5.95617 22H7.87229C8.55123 22 9.103 21.4562 9.10792 20.7822L9.13478 20.7733Z" fill="#90959F"/>
+                        </svg>
+                    </div>
+                    <span class="nav-text">Tableau de bord</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="index.html">Dashboard Light</a></li>
+                    <li><a href="index-2.html">Dashboard Dark</a></li>
+                </ul>
+            </li>
+            <li><a href="task.html" class="" aria-expanded="false">
+                    <div class="menu-icon">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_113_177)">
+                                <path d="M17 4H6C4.79111 4 4 4.7 4 6V18C4 19.3 4.79111 20 6 20H18C19.2 20 20 19.3 20 18V7.20711C20 7.0745 19.9473 6.94732 19.8536 6.85355L17 4ZM17 11H7V4H17V11Z" fill="#90959F"/>
+                                <path opacity="0.3" d="M14.5 4H12.5C12.2239 4 12 4.22386 12 4.5V8.5C12 8.77614 12.2239 9 12.5 9H14.5C14.7761 9 15 8.77614 15 8.5V4.5C15 4.22386 14.7761 4 14.5 4Z" fill="white"/>
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_113_177">
+                                    <rect width="24" height="24" fill="white"/>
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
+                    <span class="nav-text">Tasks</span>
+                </a>
+            </li>
+            <li><a href="project.html" class="" aria-expanded="false">
+                    <div class="menu-icon">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_115_172)">
+                                <path opacity="0.3" fill-rule="evenodd" clip-rule="evenodd" d="M12 4.25933C12.1489 4.25921 12.3 4.29247 12.4426 4.36281C12.6398 4.46014 12.7994 4.61977 12.8967 4.81698L14.9389 8.95491L19.5054 9.61846C20.0519 9.69788 20.4306 10.2053 20.3512 10.7519C20.3196 10.9695 20.2171 11.1706 20.0596 11.3242L16.7553 14.5451L17.5353 19.0931C17.6287 19.6374 17.2631 20.1544 16.7188 20.2478C16.502 20.2849 16.279 20.2496 16.0844 20.1473L12 18V4.25933Z" fill="#4E5566"/>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M12 4.25933V18L7.91559 20.1473C7.42675 20.4043 6.82212 20.2163 6.56512 19.7275C6.46278 19.5328 6.42746 19.3099 6.46464 19.0931L7.24469 14.5451L3.94036 11.3242C3.54487 10.9387 3.53678 10.3055 3.92228 9.91006C4.07579 9.75258 4.27693 9.65009 4.49457 9.61846L9.06104 8.95492L11.1032 4.81699C11.2773 4.46426 11.6316 4.25961 12 4.25933Z" fill="#90959F"/>
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_115_172">
+                                    <rect width="24" height="24" fill="white"/>
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
+                    <span class="nav-text">Projects</span>
+                </a>
+            </li>
+
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <div class="menu-icon">
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_14_1294)">
+                                <path d="M12.1498 18.1013H8.30563L7.86828 15.8455H5.15205L4.71469 18.1013H1.05469L3.67883 3.80664H9.52563L12.1498 18.1013ZM7.43092 13.1523L6.62526 8.04211H6.46413L5.65847 13.1523H7.43092Z"/>
+                                <path d="M17.4903 3.80664H13.5771V18.1013H17.4903V3.80664Z"/>
+                                <mask id="mask0_14_1294" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="6" y="0" width="21" height="20">
+                                    <path d="M16.6266 0.00350988L6.97803 9.84375L16.8183 19.4923L26.4668 9.65207L16.6266 0.00350988Z"/>
+                                </mask>
+                                <g mask="url(#mask0_14_1294)">
+                                    <path d="M14.7219 3.91233L14.8299 14.8991L18.7149 14.8609L18.609 4.09213L16.632 0.00825376L14.7219 3.91233ZM18.281 14.4355L15.2553 14.4652L15.1592 4.68853L15.3838 4.90622L15.8065 4.47173L16.2438 4.89717L16.6711 4.46324L17.1063 4.89177L17.5245 4.45487L17.9687 4.88023L18.1848 4.65719L18.281 14.4355ZM18.1324 4.09379L17.9573 4.27443L17.5112 3.84721L17.0972 4.27979L16.6664 3.85554L16.2373 4.29131L15.7981 3.86408L15.3748 4.29917L15.1532 4.08445L15.1525 4.00974L16.0974 2.07838L16.6448 2.43559L17.1599 2.08482L18.1324 4.09379ZM16.6397 1.91931L16.2877 1.68959L16.6299 0.990098L16.9707 1.69393L16.6397 1.91931Z"/>
+                                    <path d="M14.8359 15.545L18.7114 15.5068L18.7072 15.0772L14.8316 15.1153L14.8359 15.545Z"/>
+                                    <path d="M14.8521 17.1728C14.8573 17.7 15.2905 18.1248 15.8178 18.1196L17.7798 18.1003C18.0352 18.0978 18.2744 17.996 18.4532 17.8136C18.632 17.6312 18.7291 17.3901 18.7266 17.1347L18.7127 15.7235L14.8383 15.7616L14.8521 17.1728ZM18.297 17.1389C18.2984 17.2796 18.2449 17.4123 18.1465 17.5128C18.048 17.6132 17.9163 17.6693 17.7756 17.6707L15.8136 17.69C15.5232 17.6929 15.2846 17.4589 15.2818 17.1686L15.2721 16.1871L18.2874 16.1574L18.297 17.1389Z"/>
+                                </g>
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_14_1294">
+                                    <rect width="20" height="20"/>
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </div>
+                    <span class="nav-text">AIKit</span><span class="badge badge-xs style-1 new-badge">New</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="aikit/auto-write.html">Auto Write</a></li>
+                    <li><a href="aikit/scheduled.html">Scheduled</a></li>
+                    <li><a href="aikit/repurpose.html">Repurpose</a></li>
+                    <li><a href="aikit/rss.html">RSS</a></li>
+                    <li><a href="aikit/chatbot.html">Chatbot</a></li>
+                    <li><a href="aikit/fine-tune-models.html">Fine Tune Models</a></li>
+                    <li><a href="aikit/prompt.html">Prompt</a></li>
+                    <li><a href="aikit/setting.html">Setting</a></li>
+                    <li><a href="aikit/import.html">Import</a></li>
+                </ul>
+            </li>
+            <li><a class="has-arrow " href="javascript:void(0);" aria-expanded="false">
+                    <div class="menu-icon">
+                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g opacity="0.5">
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M15.2428 4.73756C15.2428 6.95855 17.0459 8.75902 19.2702 8.75902C19.5151 8.75782 19.7594 8.73431 20 8.68878V16.6615C20 20.0156 18.0215 22 14.6624 22H7.34636C3.97851 22 2 20.0156 2 16.6615V9.3561C2 6.00195 3.97851 4 7.34636 4H15.3131C15.2659 4.243 15.2423 4.49001 15.2428 4.73756ZM13.15 14.8966L16.0078 11.2088V11.1912C16.2525 10.8625 16.1901 10.3989 15.8671 10.1463C15.7108 10.0257 15.5122 9.97345 15.3167 10.0016C15.1211 10.0297 14.9453 10.1358 14.8295 10.2956L12.4201 13.3951L9.6766 11.2351C9.51997 11.1131 9.32071 11.0592 9.12381 11.0856C8.92691 11.1121 8.74898 11.2166 8.63019 11.3756L5.67562 15.1863C5.57177 15.3158 5.51586 15.4771 5.51734 15.6429C5.5002 15.9781 5.71187 16.2826 6.03238 16.3838C6.35288 16.485 6.70138 16.3573 6.88031 16.0732L9.35125 12.8771L12.0948 15.0283C12.2508 15.1541 12.4514 15.2111 12.6504 15.1863C12.8494 15.1615 13.0297 15.0569 13.15 14.8966Z" fill="white"/>
+                                <circle opacity="0.4" cx="19.5" cy="4.5" r="2.5" fill="white"/>
+                            </g>
+                        </svg>
+                    </div>
+                    <span class="nav-text">Charts</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="chart-flot.html">Flot</a></li>
+                    <li><a href="chart-morris.html">Morris</a></li>
+                    <li><a href="chart-chartjs.html">Chartjs</a></li>
+                    <li><a href="chart-chartist.html">Chartist</a></li>
+                    <li><a href="chart-sparkline.html">Sparkline</a></li>
+                    <li><a href="chart-peity.html">Peity</a></li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
